507c9e3f0553a8d52b6136dac85220bf9fbd1cab
muldrik <muldrik@yandex.ru>
2022-02-17 02:33:22 +0300

    Remove build dirs from git

--- use-plugin/src/test/kotlin/ru/spbu/muldrik/ShallowSizeTest.kt
+++ use-plugin/src/test/kotlin/ru/spbu/muldrik/ShallowSizeTest.kt
@@ -10,11 +10,21 @@ val pointerSize = with(System.getProperty("sun.arch.data.model")) {
   else return@with 4
 }
 
+open class Par() {
+  var reallyChar: Char = 'a'
+}
+
+data class Mda(val zoz: Int): Par()
+
 class ShallowSizeTests {
   @Test
   fun `method exists`() {
     val x = MyClass(11)
     val y = Hranilka('z')
+    val z = Mda(1)
+    z::class.memberProperties.forEach {
+      println(it)
+    }
     assertEquals(2*Int.SIZE_BYTES, x.shallowSize())
     assertEquals(pointerSize + Char.SIZE_BYTES, y.shallowSize())
   }
